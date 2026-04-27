d8df5b724d8227d17a26ce8339af86b3873318ba
Goncalo-Ferreira-dev <a14630@oficina.pt>
2026-04-27 11:56:58 +0000

    commit

---
+++ csharptest/Pages/Infopais.cshtml.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using csharptest.Models;
+using System.Text.Json;
+
+namespace csharptest.Pages
+{
+    public class InfopaisModel : PageModel {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public InfopaisModel(IHttpClientFactory httpClientFactory) {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public Pais InfoPais { get; set; }
+        public string CodigoPais { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string cod) {
+            CodigoPais = cod;
+            var client = _httpClientFactory.CreateClient("RestCountries");
+
+            var response = await client.GetAsync($"countriesfile/{cod}");
+            if (!response.IsSuccessStatusCode) {
+                return NotFound();
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+            var artigoResponse = JsonSerializer.Deserialize<List<CountryApiResponse>>(json, options)?.FirstOrDefault();
+
+            InfoPais = new Pais {
+                OfficialName = artigoResponse.name?.official,
+                Cca2 = artigoResponse.cca2,
+                FlagUrl = artigoResponse.flags?.png
+            };
+
+            return Page();
+        }
+    }
+}
